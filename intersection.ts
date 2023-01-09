ec44b1945f838e4c616c98398529c98035655779
Travis Bennett <t_bennett_a@protonmail.com>
2023-01-09 00:16:23 -0500

    Updated code to include interface and types

--- intersection.ts
+++ intersection.ts
@@ -1,9 +1,22 @@
 interface arr {
-	firstArray: number[];
-	secondArray: number[];
+	(firstArray: number[], secondArray: number[]): number[];
 }
 
 const firstArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const secondArray = [2, 4, 6, 8, 10];
+let result: number[] = [];
 
-const intersection = (firstArray, secondArray) => {};
+
+
+const intersection:arr = (firstArray, secondArray) => {
+    for (let i = 0; i < firstArray.length; i++) {
+        for (let j = 0; j < secondArray.length; j++) {
+            if (firstArray[i] === secondArray[j]) {
+                result.push(firstArray[i]);
+            }
+        }
+    }
+    return result;
+};
+
+result = intersection(firstArray, secondArray);
